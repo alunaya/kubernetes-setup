43cc479ff792effb7981fe6a9908a2b5536d1669
alunaya <33818729+alunaya@users.noreply.github.com>
2021-06-09 10:26:10 +0700

    fix proxy setting in install script

--- Vagrant-alma/config-proxy.sh
+++ Vagrant-alma/config-proxy.sh
@@ -24,6 +24,6 @@ echo 'proxy=http://10.61.11.42:3128' | tee -a /etc/yum.conf
 cat << EOF >> /home/vagrant/.bashrc
 export http_proxy=http://10.61.11.42:3128
 export https_proxy=http://10.61.11.42:3128
-export no_proxy=localhost,127.0.0.1,ansiblemaster,kubemaster,worker,vmhost
+export no_proxy=localhost,127.0.0.1,alma-ansiblemaster,alma-kubemaster,alma-kubeworker1,vmhost
 EOF
 fi
